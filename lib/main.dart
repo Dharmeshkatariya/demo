dde30a2784e55cb886a7048c60d58c674eeffe08
katariya dharmesh <katariyadharmesh658@gmail.com>
2023-02-21 16:42:07 +0530

    description

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'package:apidata/model/userdata.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _getData();
+    super.initState();
+  }
+  _getData()async{
+
+    var client = http.Client();
+    var url = "https://jsonplaceholder.typicode.com/users";
+    var uri = Uri.parse(url);
+    var response = await client.get(uri);
+    print(response);
+
+    if(response.statusCode == 200){
+      List<dynamic> uList = jsonDecode(response.body);
+
+      for(int i=0;i<uList.length;i++){
+        Map<String,dynamic> userMap = uList[i];
+        print(userMap);
+      }
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:Container(),
+
+    );
+  }
+}
---
+++ lib/model/userdata.dart
@@ -0,0 +1,100 @@
+class UserData {
+ UserData();
+  late final int id;
+  late final String name;
+  late final String username;
+  late final String email;
+  late final Address address;
+  late final String phone;
+  late final String website;
+  late final Company company;
+
+  UserData.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address = Address.fromJson(json['address']);
+    phone = json['phone'];
+    website = json['website'];
+    company = Company.fromJson(json['company']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['id'] = id;
+    _data['name'] = name;
+    _data['username'] = username;
+    _data['email'] = email;
+    _data['address'] = address.toJson();
+    _data['phone'] = phone;
+    _data['website'] = website;
+    _data['company'] = company.toJson();
+    return _data;
+  }
+}
+
+class Address {
+  Address();
+  late final String street;
+  late final String suite;
+  late final String city;
+  late final String zipcode;
+  late final Geo geo;
+
+  Address.fromJson(Map<String, dynamic> json){
+    street = json['street'];
+    suite = json['suite'];
+    city = json['city'];
+    zipcode = json['zipcode'];
+    geo = Geo.fromJson(json['geo']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['street'] = street;
+    _data['suite'] = suite;
+    _data['city'] = city;
+    _data['zipcode'] = zipcode;
+    _data['geo'] = geo.toJson();
+    return _data;
+  }
+}
+
+class Geo {
+  Geo();
+  late final String lat;
+  late final String lng;
+
+  Geo.fromJson(Map<String, dynamic> json){
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['lat'] = lat;
+    _data['lng'] = lng;
+    return _data;
+  }
+}
+
+class Company {
+ Company();
+  late final String name;
+  late final String catchPhrase;
+  late final String bs;
+
+  Company.fromJson(Map<String, dynamic> json){
+    name = json['name'];
+    catchPhrase = json['catchPhrase'];
+    bs = json['bs'];
+  }
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['name'] = name;
+    _data['catchPhrase'] = catchPhrase;
+    _data['bs'] = bs;
+    return _data;
+  }
+}
